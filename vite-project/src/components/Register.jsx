4789727f84b4d9daefe4781d146a62f5e5d5e47d
Jahnavi18104 <jahnavimeesala333@gmail.com>
2025-02-19 13:14:21 +0530

    Update Register.jsx

--- vite-project/src/components/Register.jsx
+++ vite-project/src/components/Register.jsx
@@ -280,7 +280,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("http://recler-backend-2.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
